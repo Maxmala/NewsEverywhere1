158beafaa58de03a0a8400db6516f535c1619db6
sam13008 <ikherbane.samir@gmail.com>
2018-06-04 17:56:02 +0200

    maps and google

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'home#homepage'
+  get "/index", to: "home#index" , as: "show"
   mount ForestLiana::Engine => '/forest'
+  resources :home
   
 end
