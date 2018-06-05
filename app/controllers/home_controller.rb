4e4aa56027642247eb0dd80e62afea9fd5690329
Audrey Couture <audrey.couture.bem@gmail.com>
2018-06-05 11:22:52 +0200

    hh

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,15 +4,7 @@ class HomeController < ApplicationController
   require 'news-api'
 
   def index
-  	newsapi = News.new(ENV["API_NEWS_KEY"])
-  	@all_articles = newsapi.get_everything(q: 'bitcoin',
-                                      sources: 'bbc-news,the-verge',
-                                      domains: 'bbc.co.uk,techcrunch.com',
-                                      from: '2018-04-01',
-                                      to: '2017-06-04',
-                                      language: 'en',
-                                      sortBy: 'relevancy',
-                                      page: 2)
+
 
 
 end
