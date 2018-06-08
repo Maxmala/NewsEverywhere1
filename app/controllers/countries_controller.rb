4f0a3c51a59765ba3ad2b5b023239dfe0a1753ba
Audrey Couture <audrey.couture.bem@gmail.com>
2018-06-08 19:04:44 +0200

    footer

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -4,6 +4,7 @@ class CountriesController < ApplicationController
   	#afficher params id
   	@country = Country.find_by(id: params[:id])
   	@name = @country.name
+  	@articles = Article.where(country_id: @country.id)
   	
   
 
