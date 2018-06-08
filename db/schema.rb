4ea82d7039c958605838b11db74ca6b3db831775
Damien Palierne <damien.palierne@yahoo.com>
2018-06-08 11:41:58 +0200

    add continent seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_07_124514) do
+ActiveRecord::Schema.define(version: 2018_06_08_090945) do
 
   create_table "articles", force: :cascade do |t|
     t.string "source"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2018_06_07_124514) do
     t.string "shortcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "continent"
   end
 
   create_table "countries_users", id: false, force: :cascade do |t|
