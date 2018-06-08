f9baf4ad50808aa77e65317622dd9b509e5a9bed
Audrey Couture <audrey.couture.bem@gmail.com>
2018-06-08 18:38:14 +0200

    show country

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :category
   belongs_to :country
+  attr_accessor :source, :author, :description, :url, :urlToImage, :category_id, :country_id
+  validates :description, presence: true
 end
