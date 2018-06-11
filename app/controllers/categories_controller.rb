686b5300e278e2493a4c577be9dc8f42ec84c311
Audrey Couture <audrey.couture.bem@gmail.com>
2018-06-11 20:41:51 +0200

    update

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,41 @@
 class CategoriesController < ApplicationController
-  def show
+
+    def show
+  	#afficher params id
+  	@category = category.find_by(id: params[:id])
+  	@name = @category.name
+  	@articles = @category.articles
+  	image
+  	translate
+ 
+
+  end
+
+  private
+
+  def image
+
+  	case 
+  	when @category.name == "Business"
+  		then @image = "Africa.jpg"
+  	when @country.continent == "Amérique"
+  		then @image = "America.jpg"
+  	when @country.continent == "Asie"
+  		then @image = "Asia.jpg"
+  	when @country.continent == "Europe"
+  		then @image = "Europa.jpg"
+  	when @country.continent == "Océanie"
+  		then @image = "Oceania.jpg"
+  	else
+  		@image = "Europa.jpg"
+  	end
+  	
+
+  end
+
+  def translate
+  	EasyTranslate.api_key = ENV['EASY_TRANSLATE']
+  	
+
   end
 end
