ab38976514d6466c930ceaed3f2efbe91b341cc4
sam13008 <ikherbane.samir@gmail.com>
2018-06-11 11:58:15 +0200

    debug

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :category
   belongs_to :country
-  attr_accessor :source, :author, :description, :url, :urlToImage, :category_id, :country_id
   validates :description, presence: true
   validates :url, presence: true
   
