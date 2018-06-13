9146bdbb1bb467906cd585a4b1acd7a0f59eeabd
sam13008 <ikherbane.samir@gmail.com>
2018-06-13 10:20:53 +0200

    modif

--- app/controllers/profile_pages_controller.rb
+++ app/controllers/profile_pages_controller.rb
@@ -1,5 +1,15 @@
 class ProfilePagesController < ApplicationController
 	before_action :authenticate_user!
+
   def index
+  @categorie = Category.all
+  puts params[:interests]
+  end
+
+  def create
+	
+
+  	
   end
 end
+
