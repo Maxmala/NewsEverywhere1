4b4323c2e1141d9fbf37599b33cb3cbc53e6b72e
Audrey Couture <audrey.couture.bem@gmail.com>
2018-06-14 18:49:06 +0200

    settings

--- app/controllers/profile_pages_controller.rb
+++ app/controllers/profile_pages_controller.rb
@@ -2,8 +2,61 @@ class ProfilePagesController < ApplicationController
 	before_action :authenticate_user!
 
   def index
+    @user = current_user
+    @categories = @user.category_ids
+    @countries = @user.country_ids
+    @articles = []
+
+    case 
+    when @categories.any?  && @countries.any?
+      @categories.each do |cate|
+        @countries.each do |coun|
+          @articles.concat(Article.where(category_id: cate, country_id: coun).pluck(:id))
+        end
+      end
+      if @articles.length > 20
+          @articles = (@articles.shuffle!).take(20)
+          return @articles
+      else
+        return @articles
+      end
+
+    when @categories.any? 
+      @countries = Country.all.pluck(:id)
+      @categories.each do |cate|
+        @countries.each do |coun|
+          @articles.concat(Article.where(category_id: cate, country_id: coun).pluck(:id))
+        end
+      end
+        if @articles.length > 20
+          @@articles = (@articles.shuffle!).take(20)
+          return @articles
+      else
+        return @articles
+      end
+
+    when @countries.any?
+      @categories = Category.all.pluck(:id)
+      @categories.each do |cate|
+        @countries.each do |coun|
+          @articles.concat(Article.where(category_id: cate, country_id: coun).pluck(:id))
+        end
+      end
+      if @articles.length > 20
+        @articles = (@articles.shuffle!).take(20)
+          return @articles
+      else
+        return @articles
+      end
+
+    else
+      @articles = Article.all.sample(20).pluck(:id)
+      return @articles
+            
+    end
 
   end
+
 
   def create
 	
